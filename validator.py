9c0c889747cb41e3aa30b982e47328da090526a3
Aaron Fishman <uk.afishman@gmail.com>
2021-10-22 14:49:15 +0100

    simuG from python

--- validator.py
+++ validator.py
@@ -1,20 +1,41 @@
 import os
+import subprocess
 
-pipeline_path = '/home/aaronfishman/repos/btb-seq/'
-results_path = '/home/aaronfishman/validation-results/btb-seq/'
+
+reference_path = './Mycobacterium_bovis_AF212297_LT78304.fa'
+#
+def simulate_genome(output_prefix, num_snps=16000):
+    # TODO: make sure output path does not exist
+
+    output_path = output_prefix+"simulated-genome/"
+    os.makedirs(output_path, exist_ok=True)
+
+    # TODO: store stdout to a file
+    returncode = subprocess.run([
+        "simuG.pl",
+        "-refseq", reference_path,
+        "-snp_count", str(num_snps),
+        "-prefix", output_path+"simulated"
+    ]).returncode
+
+    if returncode:
+        raise Exception("simuG failed with exit code ", returncode)
 
 def main():
+    pipeline_path = '/home/aaronfishman/repos/btb-seq/'
+    results_path = '/home/aaronfishman/validation-results/btb-seq/'
+
     # Housekeeping
     # make 
-    if os.path.isdir(results_path):
-        raise Exception("Output results path already exists")
+    # if os.path.isdir(results_path):
+    #     raise Exception("Output results path already exists")
+    # os.makedirs(results_path)
 
     if not os.path.isdir(pipeline_path):
-        raise Exception("Pipeline code repository not found")
-
-    os.makedirs(results_path)
+        raise Exception("Pipeline code repository not found")  
 
     # simuG - simulate the genome
+    simulate_genome(results_path)
 
     # read simulation -- chop up that genome
 
