6067ae9536170b0d1d3a015a98e312677711f48c
Aaron Fishman <uk.afishman@gmail.com>
2021-10-28 15:13:52 +0100

    comparing snps

--- compare_snps.py
+++ compare_snps.py
@@ -1,9 +1,33 @@
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 
-    # pipeline = pd.read_csv('blah.tab', delimiter='\t')
-    # simulated = pd.read_csv('AF2122.refseq2simseq.map.txt', delimiter='\t')
+from Bio import SeqIO
+# pipeline = pd.read_csv('blah.tab', delimiter='\t')
+# simulated = pd.read_csv('AF2122.refseq2simseq.map.txt', delimiter='\t')
 
+def masked_positions(mask_filepath='/home/aaronfishman/repos/btb-seq/references/Mycbovis-2122-97_LT708304.fas.rpt.regions'):
+    mask = pd.read_csv(mask_filepath, 
+        delimiter='\t', 
+        skiprows=2,
+        header=None,
+        names=["name", "start", "end", "mask"]
+    )
+
+    # TODO: This is off by one compared to the Emergency Port validation doc
+    #    why is that?
+    masked_pos = []
+    for i, row in mask.iterrows():
+        masked_pos.extend(range(row['start'], row['end']+1))
+        print('masked_pos', masked_pos[-1])
+        break
+
+    return masked_pos
+
+def load_consensus():
+    path = '/home/aaronfishman/ebs/pipeline-results/btb-seq-6/pipeline/Results_simulated_reads_22Oct21/consensus/simulated.fas'
+
+    for seq_record in SeqIO.parse(path, "fasta"):
+        return seq_record
 
 
 def analyse(simulated_snps, pipeline_snps):
@@ -12,7 +36,15 @@ def analyse(simulated_snps, pipeline_snps):
 
     simulated_pos = set(simulated['ref_start'].values)
     pipeline_pos = set(pipeline['POS'].values)
+    masked_pos = set(masked_positions())
+    
+    print('before: simulated_pos', len(simulated_pos))
+    simulated_pos -= masked_pos
+    pipeline_pos -= masked_pos
 
+    print('after: simulated_pos', len(simulated_pos))
+
+    consensus = load_consensus()
     missed = simulated_pos - pipeline_pos
 
     # TP - true positive -(the variant is in the simulated genome and correctly called by the pipeline)
@@ -23,6 +55,8 @@ def analyse(simulated_snps, pipeline_snps):
 
     # FN SNP calls (the variant is in the simulated genome but the pipeline does not call it).
     fn =  len(simulated_pos - pipeline_pos)
+
+    print("False negatives", simulated_pos - pipeline_pos)
 
 
     #### Stats
@@ -37,7 +71,6 @@ def analyse(simulated_snps, pipeline_snps):
 
     #  total number of errors (FP + FN) per million sequenced bases
     total_errors = fp+fn
-
 
     ### Output
     stats = {
@@ -55,4 +88,10 @@ def analyse(simulated_snps, pipeline_snps):
     a = 1
 
 if __name__ == '__main__':
-    main()
+    root = '/home/aaronfishman/ebs/pipeline-results/btb-seq-6/'
+
+    simulated_snps_filepath = root + 'simulated-genome/simulated.refseq2simseq.map.txt'
+    pipeline_snps_filepath = root + 'pipeline/Results_simulated_reads_22Oct21/snpTables/simulated.tab'
+
+    stats = analyse(simulated_snps_filepath, pipeline_snps_filepath)
+    print("stats", stats)
