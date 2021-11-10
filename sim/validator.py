201682dfd8b21ad3a4cb51ad24ac9daf55b56425
Nick Pestell <nicholas.pestell@apha.gov.uk>
2021-11-10 14:20:15 +0000

    optional use of predefined SNPs and user defined snp count

--- sim/validator.py
+++ sim/validator.py
@@ -30,14 +30,25 @@ def run(cmd, *args, **kwargs):
             cmd failed with exit code %i
           *****""" % (cmd, returncode))
 
-def simulate_genome(reference_path, output_path, num_snps=16000):
-    run([
-        "simuG.pl",
-        "-refseq", reference_path,
-        "-snp_count", str(num_snps),
-        "-prefix", output_path + "simulated"
-    ])
-
+def simulate_genome(predef_snp_path, reference_path, output_path, num_snps=16000, seed=1):
+    if predef_snp_path:
+        run([
+            "simuG.pl",
+            "-refseq", reference_path,
+            "-snp_vcf", predef_snp_path,
+            # below line tells simuG to also simulate predefined indels.
+            #"-indel_vcf", predef_snp_path, 
+            "-prefix", output_path + "simulated",
+            "-seed", str(seed)
+        ])
+    else:
+        run([
+            "simuG.pl",
+            "-refseq", reference_path,
+            "-snp_count", str(num_snps),
+            "-prefix", output_path + "simulated",
+            "-seed", str(seed)
+        ])
 
 def simulate_reads(
     genome_fasta,
@@ -91,10 +102,12 @@ def btb_seq(btb_seq_directory, reads_directory, results_directory):
          results_directory], cwd=btb_seq_directory)
 
 
-def performance_test(results_path, btb_seq_path, reference_path, exist_ok=False, branch=None):
+def performance_test(predef_snp_path, num_snps, results_path, btb_seq_path, reference_path, exist_ok=False, branch=None):
     """ Runs a performance test against the pipeline
 
         Parameters:
+            predef_snp_path (str): Path to gzipped VCF (vcf.gz) file containing predefined SNPs from snippy
+            num_snps (int): Number of manually introduced SNPs, not used if predef_snp_path parameter is set
             btb_seq_path (str): Path to btb-seq code is stored
             results_path (str): Output path to performance test results
             reference_path (str): Path to reference fasta
@@ -104,6 +117,7 @@ def performance_test(results_path, btb_seq_path, reference_path, exist_ok=False,
         Returns:
             None
     """
+
     # Add trailing slash
     btb_seq_path = os.path.join(btb_seq_path, '')
     results_path = os.path.join(results_path, '')
@@ -115,6 +129,9 @@ def performance_test(results_path, btb_seq_path, reference_path, exist_ok=False,
 
     if not os.path.isdir(btb_seq_path):
         raise Exception("Pipeline code repository not found")
+
+    if predef_snp_path and not os.path.isfile(predef_snp_path):
+        raise Exception("predfined SNPs file not found")
 
     # Output Directories
     simulated_genome_path = results_path + 'simulated-genome/'
@@ -131,7 +148,7 @@ def performance_test(results_path, btb_seq_path, reference_path, exist_ok=False,
     # Create Output Directories
     os.makedirs(simulated_genome_path, exist_ok=exist_ok)
     os.makedirs(simulated_reads_path, exist_ok=exist_ok)
-    os.makedirs(btb_seq_results_path, exist_ok=exist_ok)    
+    os.makedirs(btb_seq_results_path, exist_ok=exist_ok)
 
     # Backup btb-seq code
     # TODO: exclude the work/ subdirectory from this operation.
@@ -145,7 +162,7 @@ def performance_test(results_path, btb_seq_path, reference_path, exist_ok=False,
         checkout(btb_seq_backup_path, branch)
 
     # Run Simulation
-    simulate_genome(reference_path, simulated_genome_path)
+    simulate_genome(predef_snp_path, reference_path, simulated_genome_path, num_snps)
     simulate_reads(fasta_path, simulated_reads_path)
     btb_seq(btb_seq_backup_path, simulated_reads_path, btb_seq_results_path)
 
@@ -168,13 +185,15 @@ def main():
         description="Performance test btb-seq code")
     parser.add_argument("results", help="path to performance test results")
     parser.add_argument("--btb_seq", default="../", help="path to btb-seq code")
-    parser.add_argument("--ref", "-r", help="path to reference fasta", default=DEFAULT_REFERENCE_PATH)
-    parser.add_argument("--branch", help="path to reference fasta", default=None)
+    parser.add_argument("--branch", help="name of btb-seq branch to use", default=None)
+    parser.add_argument("--predef_snps", help="optional path to VCF file with predefined SNPs", default=None)
+    parser.add_argument("--num_snps", help="number of simulated snps, overridden by predef_snps", default=16000)
+    parser.add_argument("--ref", "-r", help="optional path to reference fasta", default=DEFAULT_REFERENCE_PATH)
 
     args = parser.parse_args(sys.argv[1:])
 
     # Run
-    performance_test(args.results, args.btb_seq, args.ref, args.branch)
+    performance_test(args.predef_snps, args.num_snps, args.results, args.btb_seq, args.ref, args.branch)
 
 if __name__ == '__main__':
     main()
