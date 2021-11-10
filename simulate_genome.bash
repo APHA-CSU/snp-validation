6aa757a755c5af21cd9b936500e0e5948bc27bd4
Aaron Fishman <uk.afishman@gmail.com>
2021-11-10 13:54:39 +0000

    Removing outdated simulation code

--- simulate_genome.bash
+++
@@ -1,5 +0,0 @@
-
-ref=~/repos/btb-seq/references/Mycbovis-2122-97_LT708304.fas.gz
-ref=./Mycbovis-2122-97_LT708304.fas.gz
-
-simuG.pl -refseq $ref -snp_count 16000 -prefix simSampleX -titv_ratio 0.5
