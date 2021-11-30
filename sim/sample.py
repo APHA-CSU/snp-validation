b310f8b98d4c28d66a35496ab0fa8512af31a311
Nick Pestell <nicholas.pestell@apha.gov.uk>
2021-11-30 16:32:19 +0000

    docstrings

--- sim/sample.py
+++ sim/sample.py
@@ -12,7 +12,7 @@ class Sample:
         return f"unnamed-{str(id(self))}"
 
     def simulate_genome(self):
-        raise NotImplementedError()
+        raise NotImplementedError("Please impliment this method")
 
     def _simulate_genome_base(self, reference_path, simulated_genome_path, params):
         cmd = ["simuG.pl",
@@ -33,7 +33,22 @@ class Sample:
         indel_mutation_fraction=0,
         indel_extension_probability=0
     ):   
+        """ Simulates raw illumina reads from genome fasta file 
 
+            Parameters:
+                simulated_genome_path (str): path to simulated genome fasta file
+                simulated_reads_path (str): path to output simulated reads fastq files
+                read_length (int): number of bases for each read
+                seed (int): the seed value for simulation
+                outer_distance (int):
+                random_dna_probability (int):
+                rate_of_mutations (int):
+                indel_mutation_fraction (int):
+                indel extension probability (int):
+
+            Returns:
+                None  
+        """
         genome_fasta_path = simulated_genome_path + self.name + '.simulated.simseq.genome.fa'
 
         output_prefix = simulated_reads_path + self.name
@@ -85,10 +100,10 @@ class VcfSample(Sample):
         if os.path.isfile(predef_snp_path):
             self.predef_snp_path = predef_snp_path
         else:
-            raise Exception("Cant Find the pipeline's snps table")
+            raise Exception("Cant find the pre-defined snp path")
         
         # extract filename
-        basename = os.path.splitext(os.path.basename(self.predef_snp_path))
+        basename = os.path.splitext(os.path.basename(self.predef_snp_path))[0]
         #TODO: this pattern is getting somewhat unweildy.
         #   etiher use something more general or save metadata separately
         self._name = f"{type(self).__name__}-{basename}-seed{self.seed}-error{self.per_base_error_rate}-readpairs-{self.num_read_pairs}"
@@ -119,13 +134,12 @@ class VcfSample(Sample):
         """
         if not os.path.isfile(self.predef_snp_path):
             raise(FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.predef_snp_path))
-        # TODO better solution for storing decomposed VCF
         tmp_decomposed_vcf_path = simulated_genome_path + '.decomposed.vcf'
         # Decompose complex SNPs
         self._decompose_complex_snps(tmp_decomposed_vcf_path)
         params = ["-snp_vcf", tmp_decomposed_vcf_path, 
-                "-indel_vcf", tmp_decomposed_vcf_path,
-                "-seed", str(seed)]
+                  "-indel_vcf", tmp_decomposed_vcf_path,
+                  "-seed", str(seed)]
         self._simulate_genome_base(reference_path, simulated_genome_path, params)
         # clean tmp decomposed VCF
         run(['rm', tmp_decomposed_vcf_path])
