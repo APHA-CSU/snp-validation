1d7e5eb3cbb5453092b2e5d25fbe6574b6d00b8f
Nick Pestell <nicholas.pestell@apha.gov.uk>
2021-12-01 17:06:03 +0000

    revert read pairs back to 10x coverage

--- sim/sample.py
+++ sim/sample.py
@@ -88,7 +88,7 @@ class VcfSample(Sample):
                  predef_snp_path, 
                  seed=1, 
                  per_base_error_rate="0",
-                 num_read_pairs = 289994,
+                 num_read_pairs = 144997,
                 ):
         
         self.seed = seed
@@ -146,7 +146,7 @@ class RandomSample(Sample):
                  num_indels=1600, 
                  seed=1, 
                  per_base_error_rate="0",
-                 num_read_pairs = 289994,
+                 num_read_pairs = 144997,
                  ):
         
         self.num_snps = num_snps
