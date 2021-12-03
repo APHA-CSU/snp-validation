29843736b2a81eb407ffc045997a99f9a88640be
Nick Pestell <nicholas.pestell@apha.gov.uk>
2021-12-03 16:28:13 +0000

    docstrings

--- sim/sample.py
+++ sim/sample.py
@@ -180,6 +180,15 @@ class RandomSample(Sample):
 
 
 def vcf_samples(datasets, snippy_dir='/mnt/fsx-027/snippy/'):
+    """ Returns a list of VcfSample objects built from data sets contained
+        in the datasets parameter. Each object is instantiated with a 
+        unique seed value
+
+        Parameters:
+            datasets (tuple (str)): elements describe the base directory name
+                for each dataset
+            snippy_dir (str): path of parent directory of datasets
+    """
     vcf_filepaths = []
     for set_i in datasets:
         vcf_dir = snippy_dir + set_i
@@ -199,20 +208,35 @@ def vcf_samples(datasets, snippy_dir='/mnt/fsx-027/snippy/'):
     return samples
 
 def quick_samples():
+    """ Returns a list containing a single RandomSample object
+    """
     return [RandomSample(seed=1)]
 
 def random_samples():
+    """ Returns a list containing two RandomSample objects 
+    """
     return [RandomSample(seed=1, per_base_error_rate="0.001-0.01"),
             RandomSample(seed=666, per_base_error_rate="0.001-0.01")]
 
 def standard_samples():
-    return vcf_samples(('standard',)) + random_samples()
+    """ Returns a list of Sample objects for the standard dataset
+    """
+    samples = vcf_samples(('standard',)) + random_samples() 
+    return samples
 
 def aph_samples():
+    """ Returns a list of Sample objects for the aph dataset
+    """
     return vcf_samples(('aph',))
 
 def zwyer_samples():
+    """ Returns a list of Sample objects for the zwyer dataset 
+    """
     return vcf_samples(('zwyer',))
 
 def all_samples():
-    return vcf_samples(('aph', 'zwyer')) + random_samples()
+    """ Returns a list of Sample objects for all VCF samples and two
+        random samples
+    """
+    samples = vcf_samples(('aph', 'zwyer')) + random_samples()
+    return samples
