cf75bd19d4bb2f434128f7b0fabe519955068fa3
Aaron Fishman <uk.afishman@gmail.com>
2021-12-08 18:07:28 +0000

    removing temp file

--- sim/temp.bash
+++
@@ -1,4 +0,0 @@
-# 'bcftools query -f '%INFO/AD{0}, %INFO/AD{1}, %INFO/DP\n' /home/aaronfishman/temp/filtered.vcf
-
-bcftools query -f '%POS, %INFO/AD{0}, %INFO/AD{1}, %INFO/DP\n' \
-'/home/aaronfishman/temp/sites1/btb-seq-results/Results_simulated-reads_08Dec21/vcf/RandomSample-snps16000-indels1600-seed1.vcf.gz'
