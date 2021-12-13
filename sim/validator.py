c76e2eead426cd2f22713cb57bc20152661f9772
Nick Pestell <nicholas.pestell@apha.gov.uk>
2021-12-13 17:44:18 +0000

    seperate simulations from performance_tests

--- sim/validator.py
+++ sim/validator.py
@@ -17,20 +17,61 @@ def btb_seq(btb_seq_directory, reads_directory, results_directory):
     run(["bash", "./btb-seq", reads_directory,
          results_directory], cwd=btb_seq_directory)
 
-def performance_test(
-    results_path, 
-    btb_seq_path, 
+def simulations(
+    output_path,
+    samples,
     reference_path=DEFAULT_REFERENCE_PATH,
-    samples=[RandomSample(16000, 1600, 1)],
+    exist_ok=True
+):
+    """ Runs a performance test against the pipeline
+
+        Parameters:
+            output_path (str): Path to output
+            samples (list of Sample objects): Samples for building simulated reads
+            reference_path (str): Path to reference genome
+            exist_ok (bool): Whether or not to throw an error if a results directory already exists
+
+        Returns:
+            None
+    """
+
+    # Validate Input
+    if os.path.isdir(output_path):
+        raise Exception("Output path already exists")
+    os.makedirs(output_path)
+    
+    output_path = os.path.join(output_path, '')
+
+    # Output Directories
+    simulated_genome_path = output_path + 'simulated-genome/'
+    simulated_reads_path = output_path + 'simulated-reads/'
+
+    # Create Output Directories
+    os.makedirs(simulated_genome_path, exist_ok=exist_ok)
+    os.makedirs(simulated_reads_path, exist_ok=exist_ok)
+
+    # Simulate Reads
+    # TODO:could these function singatures be improved?
+    for sample in samples:
+        sample.simulate_genome(reference_path, simulated_genome_path + sample.name)
+        sample.simulate_reads(simulated_genome_path, simulated_reads_path)
+
+
+def performance_test(
+    output_path,
+    btb_seq_path, 
+    samples,
+    simulated_reads_path=None,  
     exist_ok=True, 
     branch=None
 ):
     """ Runs a performance test against the pipeline
 
         Parameters:
+            output_path (str): Path to output
             btb_seq_path (str): Path to btb-seq code is stored
-            results_path (str): Output path to performance test results
-            reference_path (str): Path to reference fasta
+            samples (list of Sample objects): Samples on which to run validation on
+            simulated_reads_path (str): path to simualted reads
             exist_ok (bool): Whether or not to throw an error if a results directory already exists
             branch (str): Checkout git branch on the repo (default None)
 
@@ -40,31 +81,31 @@ def performance_test(
 
     # Add trailing slash
     btb_seq_path = os.path.join(btb_seq_path, '')
-    results_path = os.path.join(results_path, '')
+    output_path = os.path.join(output_path, '')
 
     # # Validate Input
-    if os.path.isdir(results_path):
-        raise Exception("Output results path already exists")
-    os.makedirs(results_path)
+    if not os.path.isdir(output_path):
+        raise Exception("Output path not found")
 
     if not os.path.isdir(btb_seq_path):
         raise Exception("Pipeline code repository not found")
 
     # Output Directories
-    simulated_genome_path = results_path + 'simulated-genome/'
-    simulated_reads_path = results_path + 'simulated-reads/'
+    results_path = os.path.join(output_path + branch, '')
+    os.makedirs(results_path, exist_ok=exist_ok)
+
     btb_seq_backup_path = results_path + 'btb-seq/'
     btb_seq_results_path = results_path + 'btb-seq-results/'
     site_stats_path = results_path + 'site-stats/'
 
-    # Paths to simulated reference genome and simulated SNPs file
+    # Paths to mask file and simulated reads directory 
     mask_filepath = btb_seq_backup_path + "references/Mycbovis-2122-97_LT708304.fas.rpt.regions"
+    if not simulated_reads_path:
+        simulated_reads_path = output_path + 'simulated-reads/'
 
     # TODO: handle dwgsim vcf files. Make sure we are taking into account variants it might generate
 
     # Create Output Directories
-    os.makedirs(simulated_genome_path, exist_ok=exist_ok)
-    os.makedirs(simulated_reads_path, exist_ok=exist_ok)
     os.makedirs(btb_seq_results_path, exist_ok=exist_ok)
     os.makedirs(site_stats_path, exist_ok=exist_ok)
 
@@ -78,12 +119,6 @@ def performance_test(
     #    the method handles automatic pulling of github branches.
     if branch:
         checkout(btb_seq_backup_path, branch)
-
-    # Simulate Reads
-    # TODO:could these function singatures be improved?
-    for sample in samples:
-        sample.simulate_genome(reference_path, simulated_genome_path + sample.name)
-        sample.simulate_reads(simulated_genome_path, simulated_reads_path)
 
     # Run the pipeline
     btb_seq(btb_seq_backup_path, simulated_reads_path, btb_seq_results_path)
@@ -101,7 +136,7 @@ def performance_test(
         if not os.path.exists(pipeline_snp_path):
             raise Exception("Cant Find the pipeline's snps table!!")
         
-        simulated_snp_path = results_path + f'simulated-genome/{sample.name}.simulated.refseq2simseq.map.txt'
+        simulated_snp_path = output_path + f'simulated-genome/{sample.name}.simulated.refseq2simseq.map.txt'
         pipeline_genome_path = pipeline_directory + f'consensus/{sample.name}_consensus.fas'
         
         if not os.path.exists(pipeline_genome_path):
@@ -126,6 +161,7 @@ def performance_test(
     path = results_path + "stats.csv"
     stats_table.to_csv(path)
 
+
 def checkout(repo_path, branch):
     run(["git", "checkout", str(branch)], cwd=repo_path)
 
@@ -133,8 +169,8 @@ def main():
     # Parse
     parser = argparse.ArgumentParser(description="Performance test btb-seq code")
     parser.add_argument("btb_seq", help="path to btb-seq code")
-    parser.add_argument("results", help="path to performance test results")
-    parser.add_argument("--branch", help="name of btb-seq branch to use", default=None)
+    parser.add_argument("output_path", help="path to performance test results")
+    parser.add_argument("--branch", help="name of btb-seq branch to use", default='master')
     parser.add_argument("--ref", "-r", help="optional path to reference fasta", default=DEFAULT_REFERENCE_PATH)
     parser.add_argument("--quick", "-q", help="Run quick samples", action='store_true')
 
@@ -146,13 +182,15 @@ def main():
     else:
         samples = standard_samples()
 
+    # Simulate reads
+    simulations(args.output_path, samples)
+
     # Run
     performance_test(
-        args.results, 
+        args.output_path, 
         args.btb_seq, 
-        args.ref,
-        samples=samples, 
-        branch=args.branch
+        samples, 
+        branch=args.branch,
     )
 
 if __name__ == '__main__':
