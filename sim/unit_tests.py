d6af33079cbfe1c54b8d8d0309de426443d9661f
Aaron Fishman <uk.afishman@gmail.com>
2021-12-16 11:23:53 +0000

    Comments

--- sim/unit_tests.py
+++ sim/unit_tests.py
@@ -14,24 +14,28 @@ class ValidatorTests(unittest.TestCase):
         """ A simple introductory unit test that checks we can run through the
             validator without errors
         """
+        # Mock I/O on validator
         validator.shutil.copytree = Mock()
         validator.os.makedirs = Mock()
         validator.os.path.exists = Mock(return_value=False)
         validator.utils.run = Mock()
         validator.glob.glob = Mock(return_value=[''])
 
-        genome.os.path.exists = Mock(return_value=True)
-
-        validator.sequenced.from_results_dir = Mock(return_value=[Mock()])
-        validator.processed.from_list = Mock(return_value=[Mock()])
         validator.compare_snps.benchmark = Mock(return_value=[Mock(),{'mock': Mock()}])
 
+        # Mock Sample
+        validator.sequenced.from_results_dir = Mock(return_value=[Mock()])
+        validator.processed.from_list = Mock(return_value=[Mock()])
+        
         mock_sample = Sample()
 
+        genome.os.path.exists = Mock(return_value=True)
         mock_genome = genome.SimulatedGenome('mock-genome', '', '', '', '')
+
         mock_sample.simulate_genome = Mock(return_value=mock_genome)
         mock_sample.simulate_reads = Mock(return_value=['READ1', 'READ2'])
 
+        # Run test
         validator.performance_test('./', './', [mock_sample])
 
 if __name__ == '__main__':
