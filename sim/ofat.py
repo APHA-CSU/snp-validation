b852021dba362fd034868d282763ad19c987e11e
CameroneN <cameron.nicholls@apha.gov.uk>
2022-01-05 09:51:05 +0000

    added plot funtion

--- sim/ofat.py
+++ sim/ofat.py
@@ -4,6 +4,11 @@ import argparse
 import glob
 import json
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy
+from pathlib import Path
+import shutil
 
 import validator
 import config
@@ -120,6 +125,30 @@ def analyse(root_path):
         "fp": fps,
         "tp": tps
     })
+
+def plot(root_path):
+
+    dirs = glob.glob(root_path + "/*/stats.csv")
+
+    values = pd.DataFrame(index =[range(1, (len(dirs)))])
+    branches=['branch:']
+
+    for directory in dirs:
+        path = Path(directory)
+        sample_name = path.parts[-2]
+        branches.append(sample_name)
+        csv = pd.read_csv(directory)
+        values[sample_name] = csv['f_score']
+
+    print(branches)
+    plt.boxplot(values, showfliers = True)
+    # y = (0.8, 0.85, 0.9, 0.95, 1)
+    #y = (0.9, 0.92, 0.94, 0.96, 0.98, 1)
+    plt.xticks(range(0, len(branches)),  branches, rotation=-20)
+    #plt.yticks(y)
+    plt.show()
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the performance benchmarking tool against a number of git branches")
