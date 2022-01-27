04c223c1bab0efac9092e691505196cf6c2e5c27
Nick Pestell <nicholas.pestell@apha.gov.uk>
2022-01-27 14:57:35 +0000

    counting all FNs when masking

--- compare_snps.py
+++ compare_snps.py
@@ -57,7 +57,6 @@ def analyse(simulated_snp_path, pipeline_snp_path, pipeline_genome_path, mask_fi
         # TPs, FPs and FNs in masked regions 
         tp = tp.difference(masked_pos)
         fp = fp.difference(masked_pos) 
-        fn = fn.difference(masked_pos)
 
     # load consensus file
     pipeline_genome = load_consensus(pipeline_genome_path)
