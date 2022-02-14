831826ff7824586ffd005a3bae9990cb82ba9b8c
CameroneN <cameron.nicholls@apha.gov.uk>
2022-02-14 16:37:12 +0000

    made changes

--- produce_snp_matrix.py
+++ produce_snp_matrix.py
@@ -1,38 +1,40 @@
 from glob import glob
 import subprocess
 import argparse
+import utils
 import os
 
 def snps(input_path, output_path):
     # glob filenames
     megafasta = ""
     paths = glob(input_path + "*_consensus.fas")
+    fasta_list = []
     for path in paths:
-        file = open(path)
-        fasta = file.read()
-        megafasta = megafasta + fasta + '\n'
+        with open(path) as file: fasta = file.read()
+        fasta_list.append(fasta)
+    megafasta = "\n".join(fasta_list)
 
+    
     if os.path.exists(output_path + 'output'):
-        os.chdir(output_path + 'output')
+        pass
     else:
         os.mkdir(output_path + 'output')
-        os.chdir(output_path + 'output')
+        
+    snpsout = output_path + 'output/'
+    with open(f'{snpsout}combined.fas', 'w') as file: 
+        file.write(megafasta)
 
-    file = open(r'combined.fas', 'w')
-    file.write(megafasta)
-    file.close()
-
+    
     # run snp sites 
-    cmd = 'snp-sites combined.fas -o snpsites.fas'
-    subprocess.run(cmd, shell=True)
-
+    cmd = f'snp-sites {snpsout}combined.fas -o {snpsout}snpsites.fas'
+    utils.run(cmd, shell=True)
     # produce vcf of snps- for investigating sites
-    cmd = 'snp-sites combined.fas -v -o snpsites.vcf'
-    subprocess.run(cmd, shell=True) 
+    cmd = f'snp-sites {snpsout}combined.fas -v -o {snpsout}snpsites.vcf'
+    utils.run(cmd, shell=True)
 
     # run snp-dists
-    cmd = 'snp-dists snpsites.fas > snps.tab'
-    subprocess.run(cmd, shell=True)
+    cmd = f'snp-dists {snpsout}snpsites.fas > {snpsout}snps.tab'
+    utils.run(cmd, shell=True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="download sample fastqs from aws from input of sample refs")
